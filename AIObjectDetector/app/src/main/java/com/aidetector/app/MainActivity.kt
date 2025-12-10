2fa2048c1322c271b9b72a12b751aeb5a6bb54e5
hamedabyah1-collab <hamed.abyah1@gmail.com>
2025-12-10 05:23:49 +0300

    Update MainActivity.kt

--- AIObjectDetector/app/src/main/java/com/aidetector/app/MainActivity.kt
+++ AIObjectDetector/app/src/main/java/com/aidetector/app/MainActivity.kt
@@ -1,81 +1,45 @@
-package com.aidetector.app
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
 
-import android.Manifest
-import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.aidetector.app.databinding.ActivityMainBinding
-import com.guolindev.permissionx.PermissionX
+    <Button
+        android:id="@+id/btn_start_detection"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="فتح الكاميرا" 
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        android:layout_marginTop="16dp"
+        android:layout_marginStart="16dp"
+        android:layout_marginEnd="16dp"/>
 
-class MainActivity : AppCompatActivity() {
+    <Button
+        android:id="@+id/btn_history"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="@string/history"
+        app:layout_constraintTop_toBottomOf="@id/btn_start_detection"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        android:layout_marginTop="8dp"
+        android:layout_marginStart="16dp"
+        android:layout_marginEnd="16dp"/>
 
-    private lateinit var binding: ActivityMainBinding
+    <Button
+        android:id="@+id/btn_settings"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="@string/settings"
+        app:layout_constraintTop_toBottomOf="@id/btn_history"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        android:layout_marginTop="8dp"
+        android:layout_marginStart="16dp"
+        android:layout_marginEnd="16dp"/>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setupClickListeners()
-    }
-
-    private fun setupClickListeners() {
-        // زر "ابدأ الكشف"
-        binding.btnStartDetection.setOnClickListener {
-            requestPermissions()
-        }
-
-        // زر "السجل"
-        binding.btnHistory.setOnClickListener {
-            startActivity(Intent(this, HistoryActivity::class.java))
-        }
-
-        // زر "الإعدادات"
-        binding.btnSettings.setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
-    }
-
-    private fun requestPermissions() {
-        // تحديد الأذونات المطلوبة
-        val permissionsList = mutableListOf(Manifest.permission.CAMERA)
-
-        // إضافة إذن التخزين حسب إصدار Android
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-            // Android 9 (Pie) وأقل
-            permissionsList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            // Android 13 (Tiramisu) وأعلى
-            permissionsList.add(Manifest.permission.READ_MEDIA_IMAGES)
-        } else {
-            // Android 10, 11, 12 لا تحتاج إلى إذن تخزين صريح لحفظ الملفات في المجلدات الخاصة بالتطبيق
-        }
-
-        PermissionX.init(this)
-            .permissions(permissionsList)
-            .onExplainRequestBefore { scope, deniedList ->
-                // عرض رسالة توضيحية قبل طلب الأذونات
-                scope.showRequestReasonDialog(
-                    deniedList,
-                    getString(R.string.permission_explanation), // "يحتاج التطبيق إلى هذه الأذونات ليعمل بشكل صحيح."
-                    getString(R.string.ok),
-                    getString(R.string.cancel)
-                )
-            }
-            .request { allGranted, grantedList, deniedList ->
-                if (allGranted) {
-                    // جميع الأذونات ممنوحة، ابدأ نشاط الكاميرا
-                    startActivity(Intent(this, CameraActivity::class.java))
-                } else {
-                    // لم يتم منح جميع الأذونات
-                    Toast.makeText(
-                        this,
-                        getString(R.string.permission_denied_message) + deniedList.toString(),
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-    }
-}
+</androidx.constraintlayout.widget.ConstraintLayout>
