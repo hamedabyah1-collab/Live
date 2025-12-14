18f3749e241012b7a5e81e33a5611a2abc0ddc5f
hamedabyah1-collab <hamed.abyah1@gmail.com>
2025-12-14 06:31:23 +0300

    Refactor MainActivity to use Kotlin instead of XML
    Removed XML layout and replaced with Kotlin code for MainActivity.

--- AIObjectDetector/app/src/main/java/com/aidetector/app/MainActivity.kt
+++ AIObjectDetector/app/src/main/java/com/aidetector/app/MainActivity.kt
@@ -1,45 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".MainActivity">
 
-    <Button
-        android:id="@+id/btn_start_detection"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:text="فتح الكاميرا" 
-        app:layout_constraintTop_toTopOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        android:layout_marginTop="16dp"
-        android:layout_marginStart="16dp"
-        android:layout_marginEnd="16dp"/>
+package com.aidetector.app
 
-    <Button
-        android:id="@+id/btn_history"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:text="@string/history"
-        app:layout_constraintTop_toBottomOf="@id/btn_start_detection"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        android:layout_marginTop="8dp"
-        android:layout_marginStart="16dp"
-        android:layout_marginEnd="16dp"/>
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-    <Button
-        android:id="@+id/btn_settings"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:text="@string/settings"
-        app:layout_constraintTop_toBottomOf="@id/btn_history"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        android:layout_marginTop="8dp"
-        android:layout_marginStart="16dp"
-        android:layout_marginEnd="16dp"/>
+class MainActivity : AppCompatActivity() {
 
-</androidx.constraintlayout.widget.ConstraintLayout>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
